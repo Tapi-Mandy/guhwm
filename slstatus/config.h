370a19c56476de10a599ad212d1935a82a13c4d8
Tapi <mandytapi@gmail.com>
2025-12-03 19:53:47 +0200

    Update config.h

--- slstatus/config.h
+++ slstatus/config.h
@@ -1,6 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* interval between updates (in ms) */
+/* Set this to 1000 (1 second) or 500. 200 is too fast for a script. */
 const unsigned int interval = 1000;
 
 /* text to show if no value can be retrieved */
@@ -63,11 +64,6 @@ static const char unknown_str[] = "n/n/n/n";
  * wifi_essid          WiFi ESSID                      interface name (wlan0)
  * wifi_perc           WiFi signal in percent          interface name (wlan0)
  */
-
-/* interval between updates (in ms) */
-/* Set this to 1000 (1 second) or 500. 200 is too fast for a script. */
-const unsigned int interval = 1000;
-
 static const struct arg args[] = {
     { run_command, "%s", "~/.config/guhwm/slstatus/slstatus.sh" },
 };
