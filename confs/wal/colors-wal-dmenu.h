19f85c53ad24d83f162b82e71ee7d2844e2ba2fc
Tapi <mandytapi@gmail.com>
2026-02-12 16:35:26 +0200

    push the bush

--- confs/wal/colors-wal-dmenu.h
+++
@@ -1,6 +0,0 @@
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#e5deda", "#4a3c33" },
-	[SchemeSel] = { "#e5deda", "#B19B8D" },
-	[SchemeOut] = { "#e5deda", "#E1CCBD" },
-};
